1a0e78e1ea9198ead2dfaabd2e27024292c5d96f
sanscomp <sanscomp@anslems-iMac.lan>
2019-08-03 20:36:55 +0100

    Style components

--- src/App.js
+++ src/App.js
@@ -9,16 +9,25 @@ function App() {
 
   const person = names ? (
     names.map((personName, index) => {
-      return <p className="app__maintext" key={index}>{personName.name}, born {personName.birth_year}</p>
+      return <div className="people__outer-container">
+        <h5 className="people__title" key={index}>{personName.name}</h5>
+        <div className="people__inner-container">
+          <p className="people__text">Birth year: {personName.birth_year}</p>
+          <p className="people__text">Height: {personName.height}cm </p>
+          <p className="people__text">Gender: {personName.gender}</p>
+        </div>
+
+      </div>
     })) : "loading..."
 
   return (
     <div className="app">
       <Header />
+      {person}
       <SearchForm onDataRecieved={(results) => {
         setNames(results);
       }} />
-      {person}
+
       <p className="app__subtext">*A person's in-universe standard date of birth (D.O.B) is either BBY or ABY.<br></br>
         Before the Battle of Yavin or After the Battle of Yavin.<br></br>
         The Battle of Yavin occurs at the end of Star Wars episode IV: A New Hope.</p>
