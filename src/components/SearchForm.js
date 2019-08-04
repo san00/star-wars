bd897afc2229ac52430ed940365f5e60d45719c2
sanscomp <sanscomp@anslems-iMac.lan>
2019-08-04 20:01:26 +0100

    Refactor code

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,22 +3,25 @@ import axios from 'axios';
 
 function SearchForm(props) {
     const [value, setValue] = useState("");
+
+    const requestData = async (e) => {
+        e.preventDefault();
+        try {
+            const apiRes = await axios
+                .get(`https://swapi.co/api/people/?search=${value}`);
+            props.handleData(apiRes.data.results);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div>
             <form className="form">
-            <label className="form__label" htmlFor="form__input">Search for a person</label>
+                <label className="form__label" htmlFor="form__input">Search for a person</label>
                 <input className="form__input" type="text" onChange={e => { setValue(e.target.value) }} placeholder="Enter name..." ></input>
-                <input className="form__input-submit" type="submit" onClick={(e) => {
-                    e.preventDefault();
-                    axios
-                        .get(`https://swapi.co/api/people/?search=${value}`)
-                        .then(apiRes => {
-                            props.onDataRecieved(apiRes.data.results)
-                        })
-                        .catch(error => {
-                            console.log(error);
-                        })
-                }} ></input>
+                <input className="form__input-submit" type="submit" onClick={e => requestData(e)} ></input>
             </form>
         </div>
     )
